bb217401dd4da5f1e7ca48d574d8014ab2b45f32
c0dev0id <sh+git@codevoid.de>
2022-04-01 13:40:17 +0200

    remove files

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -client -jar closure-compiler.jar \
+/usr/local/jdk-17/bin/java -client -jar closure-compiler.jar \
   --js js-lib/**.js \
   --js js-lib/**/*.js \
   --warning_level=VERBOSE --formatting=PRETTY_PRINT --language_in=ECMASCRIPT6 --compilation_level=ADVANCED_OPTIMIZATIONS \
